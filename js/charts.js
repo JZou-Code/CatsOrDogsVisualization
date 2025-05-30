bb7b7c0a65b3c525dbc3cf9fc2acf76292924d04
JZou-Code <jzou2871@gmail.com>
2025-05-30 22:14:03 +1200

    Debug.

--- js/charts.js
+++ js/charts.js
@@ -670,34 +670,11 @@ const dogsBtn = document.querySelector('#dogsBtn');
 //     $full.fadeTo(1000, 0);
 // })
 
-const cat = document.querySelector('#catChart');
-const dog = document.querySelector('#dogChart');
-const full = document.querySelector('#fullChart');
 
-const icons = document.querySelectorAll('#reasonChartContainer .icon');
 
-const iconCats = document.querySelector('#iconCats');
-const iconDogs = document.querySelector('#iconDogs');
 
-for (const icon of icons) {
-    icon.addEventListener('mouseout', () => {
-        cat.classList.add('hidden');
-        dog.classList.add('hidden');
-        full.classList.remove('hidden');
-    })
-}
 
-iconCats.addEventListener('mouseenter', () => {
-    cat.classList.remove('hidden');
-    dog.classList.add('hidden');
-    full.classList.add('hidden');
-})
 
-iconDogs.addEventListener('mouseenter', () => {
-    cat.classList.add('hidden');
-    dog.classList.remove('hidden');
-    full.classList.add('hidden');
-})
 
 
 (function () {
